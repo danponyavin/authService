688817652cd1e3e469bdd0e0093bd7a3decb64e9
Daniil <dan.ponyavin@yandex.ru>
2024-08-12 18:50:58 +0300

    Created simple handlers

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"AuthService/pkg/service"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -14,14 +16,41 @@ func NewHandler(services *service.Service) *Handler {
 	return &Handler{services: services}
 }
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	base := router.Group("/api/v1")
+	base.POST("auth/tokens", h.Auth)
+	base.POST("auth/refresh", h.RefreshTokens)
 
 	return router
+}
+
+type AuthRequest struct {
+	UserID uuid.UUID `json:"user_id" binding:"required"`
+	Email  string    `json:"email" binding:"required,email"`
+}
+
+func (h *Handler) Auth(c *gin.Context) {
+	var authRequest AuthRequest
+	if err := c.ShouldBind(&authRequest); err != nil {
+		c.JSON(http.StatusBadRequest, Error{Message: err.Error()})
+	}
+	fmt.Println(authRequest.UserID, authRequest.Email)
+}
+
+type RefreshTokenRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+func (h *Handler) RefreshTokens(c *gin.Context) {
+	var refreshTokenRequest RefreshTokenRequest
+	if err := c.ShouldBind(&refreshTokenRequest); err != nil {
+		c.JSON(http.StatusBadRequest, Error{Message: err.Error()})
+	}
+	fmt.Println(refreshTokenRequest.RefreshToken)
 }
